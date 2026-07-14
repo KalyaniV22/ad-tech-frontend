6aa1ce71e298fdc2dab9c199452c78dedd23b1a2
kalyanivelukar <kalyani.velukar22@gmail.com>
2026-07-14 09:57:01 +0530

    first commit

---
+++ src/main/java/com/ad_tech_fronted/AdTechFrontedApplication.java
@@ -0,0 +1,13 @@
+package com.ad_tech_fronted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdTechFrontedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdTechFrontedApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("contactForm");
+
+const API_URL = "https://ad-tech-enterprises-2.onrender.com";
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const contact = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        company: document.getElementById("company").value,
+        message: document.getElementById("message").value
+    };
+
+    fetch(`${API_URL}/contact`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(contact)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert("✅ Thank You! Your message has been submitted successfully.");
+        form.reset();
+    })
+    .catch(error => {
+        alert("Something went wrong!");
+        console.log(error);
+    });
+
+});
