8d66e9672479479996717890af5f5af8867a420c
kalyanivelukar <kalyani.velukar22@gmail.com>
2026-07-14 10:04:20 +0530

    Initial frontend project

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 const form = document.getElementById("contactForm");
 
-const API_URL = "https://ad-tech-enterprises-2.onrender.com";
-
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function(event){
 
     event.preventDefault();
 
     const contact = {
+
         name: document.getElementById("name").value,
+
         email: document.getElementById("email").value,
+
         company: document.getElementById("company").value,
+
         message: document.getElementById("message").value
+
     };
 
-    fetch(`${API_URL}/contact`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
+    fetch("http://localhost:8085/contact",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
         },
-        body: JSON.stringify(contact)
+
+        body:JSON.stringify(contact)
+
     })
+
     .then(response => response.json())
-    .then(data => {
-        alert("✅ Thank You! Your message has been submitted successfully.");
+
+    .then(data =>{
+
+alert("✅ Thank You! Your message has been submitted successfully.");
         form.reset();
+
     })
-    .catch(error => {
+
+    .catch(error =>{
+
         alert("Something went wrong!");
+
         console.log(error);
+
     });
 
 });
